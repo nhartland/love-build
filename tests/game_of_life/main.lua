1a2aa4e9ca0b38886c139d93cda86e49c58efee7
Nathan Hartland <nathan.hartland@cloudsolutions.co.uk>
2020-04-06 11:12:08 +0200

    Simplifying GoL example

--- tests/game_of_life/main.lua
+++ tests/game_of_life/main.lua
@@ -5,27 +5,20 @@ local neighbourhood = require('forma.neighbourhood')
 
 local pixels = 128
 
+-- 128*128 square
 local domain = primitives.square(pixels,pixels)
---local sample = subpattern.random(domain, math.floor(domain:size()/2))
---local moore  = automata.rule(neighbourhood.moore(), "B3/S23")
---local ruleset = {moore}
---
--- Complicated ruleset, try leaving out or adding more rules
-local sample = subpattern.random(domain, 1)
---local moore = automata.rule(neighbourhood.moore(),      "B12/S012345678")
---local diag  = automata.rule(neighbourhood.diagonal_2(), "B01/S01234")
---local vn    = automata.rule(neighbourhood.von_neumann(),"B12/S01234")
-local vn    = automata.rule(neighbourhood.von_neumann(),"B24/S23")
-local ruleset = {moore, diag}
+-- Set half the initial possible cells to active
+local sample = subpattern.random(domain, math.floor(domain:size()/2))
+-- GoL rule
+local moore  = automata.rule(neighbourhood.moore(), "B3/S23")
+local ruleset = {moore}
 
 function love.load()
     love.window.setMode(pixels, pixels)
 end
 
 function love.update()
-    for _=1, 10, 1 do
-    sample = automata.async_iterate(sample, domain, ruleset)
-    end
+    sample = automata.iterate(sample, domain, ruleset)
 end
 
 function love.draw()
