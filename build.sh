20d0348be42d3410db6ce1abe4985aa0f4f4470b
Nathan Hartland <nathan@pseudorandom.dev>
2020-04-12 16:15:03 +0200

    windows build function

--- build.sh
+++ build.sh
@@ -54,12 +54,14 @@ build_macos(){
         cd "${build_dir}" 
         # Download love for macos
         get_love_binaries "${INPUT_LOVE_VERSION}" "macos"
+
         # Copy Data
         cp "application.love" "love_macos/Contents/Resources/ "
         # If a plist file is provided, use that
         if [ -f "${INPUT_SOURCE_DIR}/Info.plist" ]; then
             cp "${INPUT_SOURCE_DIR}/Info.plist" "love_macos/Contents/"
         fi
+
         # Setup final archives
         mv "love_macos" "${INPUT_APP_NAME}.app"
         zip -ry "${INPUT_APP_NAME}_macos.zip" "${INPUT_APP_NAME}.app" 
@@ -71,14 +73,41 @@ build_macos(){
     echo "::set-output name=macos-filename::${AN}_macos.zip"
 }
 
+# Exports a zipped win32 application to the
+# result directory.
+build_windows(){
+    build_dir=$(mktemp -d -t love-build-XXXXXX)
+    build_lovefile "${build_dir}/application.love"
+    (
+        # Change to build dir (subshell to preserve cwd)
+        cd "${build_dir}" 
+        # Download love for macos
+        get_love_binaries "${INPUT_LOVE_VERSION}" "win32"
+
+        # Copy data
+        cat "love_win32/love.exe" "application.love" > "love_win32/${INPUT_APP_NAME}.exe"
+        # Delete unneeded files
+        rm "love_win32/love.exe"
+        rm "love_win32/lovec.exe"
+        rm "love_win32/love.ico"
+        rm "love_win32/changes.txt"
+        rm "love_win32/readme.txt"
+
+        # Setup final archive
+        mv "love_win32" "${INPUT_APP_NAME}_win32"
+        zip -ry "${INPUT_APP_NAME}_win32.zip" "${INPUT_APP_NAME}_win32"
+        mv "${INPUT_APP_NAME}_win32.zip" "${INPUT_RESULT_DIR}"/
+    )
+    # Cleanup build directory
+    rm -rf "${build_dir}"
+    # Export filename
+    echo "::set-output name=win32-filename::${AN}_win32.zip"
+}
+
 main() {
     
     check_environment    
 
-    # Shorten variables a little
-    AN=${INPUT_APP_NAME}
-    LV=${INPUT_LOVE_VERSION}
-    
     echo "-- LOVE build parameters --"
     echo "App name: ${AN}"
     echo "LOVE version: ${LV}"
@@ -90,38 +119,15 @@ main() {
     # Make results directory if it does not exist
     mkdir -p "${INPUT_RESULT_DIR}"
     
-    # Change CWD to the build directory and copy source files
-    BUILD_DIR=$(mktemp -d -t love-build-XXXXXX)
-    cd "${BUILD_DIR}"
-    
     ### LOVE build ####################################################
     
-    build_lovefile "${AN}.love"
-    cp "${AN}.love" "${INPUT_RESULT_DIR}"/
-    echo "::set-output name=love-filename::${AN}.love"
+    build_lovefile "${INPUT_RESULT_DIR}/${INPUT_APP_NAME}.love"
+    echo "::set-output name=love-filename::${INPUT_APP_NAME}.love"
     
     ### macos build ###################################################
     
     build_macos
-    
-    ### win32 build ###################################################
-    
-    # Download love for windows
-    get_love_binaries "${LV}" "win32"
-    mv "love_win32" "${AN}_win32"
-    # Copy data
-    cat "${AN}_win32/love.exe" "${AN}.love" > "${AN}_win32/${AN}.exe"
-    # Delete unneeded files
-    rm "${AN}_win32/love.exe"
-    rm "${AN}_win32/lovec.exe"
-    rm "${AN}_win32/love.ico"
-    rm "${AN}_win32/changes.txt"
-    rm "${AN}_win32/readme.txt"
-    # Setup final archive
-    zip -ry "${AN}_win32.zip" "${AN}_win32" && rm -rf "${AN}_win32"
-    mv "${AN}_win32.zip" "${INPUT_RESULT_DIR}"/
-    # Export filename
-    echo "::set-output name=win32-filename::${AN}_win32.zip"
+    build_windows 
 }
 
 
