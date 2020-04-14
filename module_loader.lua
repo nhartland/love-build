d54f1ab0de6e60fb22919a9907db4ccf3c73b975
Nathan Hartland <nathan@pseudorandom.dev>
2020-04-14 12:24:25 +0200

    use love require path

--- module_loader.lua
+++ module_loader.lua
@@ -2,5 +2,5 @@
 -- Code injected by LOVE Build to include the installed luarocks tree
 -- into the require path.
 
-love.filesystem.setRequirePath( "?.lua;?/init.lua;lb_modules/share/lua/5.1/?.lua;lb_modules/share/lua/5.1/?/init.lua"
+love.filesystem.setRequirePath( "?.lua;?/init.lua;lb_modules/share/lua/5.1/?.lua;lb_modules/share/lua/5.1/?/init.lua" )
 --- </love-build>
